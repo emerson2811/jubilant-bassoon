aadf3ce88b8959083f15bfafafcbdf686b08fcf4
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-21 21:02:28 -0500

    Adding prime numbers method

--- src/com/javanese/interest.java
+++ src/com/javanese/interest.java
@@ -9,13 +9,42 @@ public class interest {
 
         }
 
-        for (int i=0; i<11; i++) {
+        for (int i=8; i<20; i++) {
             System.out.println("48,4000 at " + i + " interest = " + calculateInterest(48400, i));
         }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
-        return (amount * (interestRate/100));
+        return (amount * (interestRate / 100));
 
+        }
+    {
+        int count = 0;
+        for(int i = 10; i <50; i++) {
+            if(isPrim(i)) {
+                count++;
+                System.out.println("Number" + i + "is prime");
+                if (count == 3) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isPrim (int n) {
+
+        if(n ==1) {
+            return false;
+
+        }
+
+        for(int i=2; 1 <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return false;
     }
 }
