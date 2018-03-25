8e750b6c0d2fd01a018bcae441470cc997c0dba9
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-25 12:08:59 -0500

    Created isEvenumber method.

--- src/com/javanese/Whilin.java
+++ src/com/javanese/Whilin.java
@@ -2,21 +2,52 @@ package com.javanese;
 
 public class Whilin {
 
-    public static void main(String[] args) {
-        int count = 0;
-        while (count != 6) {
-            System.out.println("count value is " + count);
-            count++;
-        }
+        public static void main(String[] args) {
+            int number = 4;
+            int finishNumber = 20;
 
-        count = 1;
-        while (true) {
-            if(count == 5) {
-                break;
+            while (number <= finishNumber) {
+                number++;
+                if (!isEvenNumber(number)) {
+                    continue;
+                }
+
+                System.out.println("Even number " + number);
+            }
+        }
+        public static boolean isEvenNumber(int number) {
+            if((number % 2) == 0) {
+                return true;
+            } else {
+                return false;
             }
 
-            System.out.println("Count value est " + count);
-            count++;
-        }
+            }
+//        int count = 6;
+//        while (count != 6) {
+//            System.out.println("count value is " + count);
+//            count++;
+//        }
+
+//        count = 1;
+//        while (true) {
+//            if(count == 5) {
+//                break;
+//            }
+//
+//            System.out.println("Count value est " + count);
+//            count++;
+//        }
+//
+//        count = 1;
+//        do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//
+//            if (count > 100) {
+//                break;
+//            }
+
+//        } while (count !=6);
     }
-}
+
