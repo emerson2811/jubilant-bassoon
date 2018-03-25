1f76b5fb079a0d7f0c7e8c5a76566fe96dcbe57c
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-25 12:49:19 -0500

    Added output strings describing odds and evens

--- src/com/javanese/Whilin.java
+++ src/com/javanese/Whilin.java
@@ -5,21 +5,22 @@ public class Whilin {
         public static void main(String[] args) {
             int number = 0;
             int finishNumber = 20;
-            int i = 0;
+            int evenNumbersFound = 0;
 
             while (number <= finishNumber) {
                 number++;
-                if (isEvenNumber(number)) {
-                    i++;
-                    System.out.println("Even number " + number);
-                    System.out.println("Total number of even numbers so far is " + i);
+                if (!isEvenNumber(number)) {
+                    System.out.println(number+ " is not an even number");
                 }
-                    else if (i==5) {
+                if (isEvenNumber(number)) {
+                    evenNumbersFound++;
+                    System.out.println(number + " IS an even number");
+                    System.out.println("Total number of even numbers so far is " + evenNumbersFound);
+                }
+                    else if (evenNumbersFound==4) {
                         break;
                     }
                 }
-
-                System.out.println("Even number " + number);
             }
 
         public static boolean isEvenNumber(int number) {
