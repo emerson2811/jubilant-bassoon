4ff6930d1150271789563cff13afcb7174bacd37
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-25 12:32:15 -0500

    Added even number counter.

--- src/com/javanese/Whilin.java
+++ src/com/javanese/Whilin.java
@@ -3,18 +3,25 @@ package com.javanese;
 public class Whilin {
 
         public static void main(String[] args) {
-            int number = 4;
+            int number = 0;
             int finishNumber = 20;
+            int i = 0;
 
             while (number <= finishNumber) {
                 number++;
-                if (!isEvenNumber(number)) {
-                    continue;
+                if (isEvenNumber(number)) {
+                    i++;
+                    System.out.println("Even number " + number);
+                    System.out.println("Total number of even numbers so far is " + i);
+                }
+                    else if (i==5) {
+                        break;
+                    }
                 }
 
                 System.out.println("Even number " + number);
             }
-        }
+
         public static boolean isEvenNumber(int number) {
             if((number % 2) == 0) {
                 return true;
